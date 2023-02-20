a1484100e6262b597c1996fa4ed1043495d18f79
ndeen17 <nurudeeny17@gmail.com>
2023-02-20 01:30:32 -0800

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("size of a char: %lu byte(s)\n", (unsigned long)size of(a));
+printf("size of a char: %lu byte(s)\n", (unsigned long)size of(b));
+printf("size of a char: %lu byte(s)\n", (unsigned long)size of(c));
+printf("size of a char: %lu byte(s)\n", (unsigned long)size of(d));
+printf("size of a char: %lu byte(s)\n", (unsigned long)size of(f));
+return (0);
+}
