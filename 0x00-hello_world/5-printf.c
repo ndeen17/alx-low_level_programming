d21bfce8bfc715736410ba381f0819b4e83312b8
ndeen17 <nurudeeny17@gmail.com>
2023-02-20 00:35:37 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+* main - A program that prints a line with printf functions
+* Return: 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
