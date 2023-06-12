eceb7dcac5d5153162a85f4ee44666db062067dd
ndeen17 <nurudeeny17@gmail.com>
2023-06-11 19:09:48 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - name, followed by a new line.
+* @argc: value  count
+* @argv: value
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+        (void) argc;
+        printf("%s\n", argv[0]);
+        return (0);
+}
